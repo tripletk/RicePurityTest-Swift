ee094a0cfb0b1941e0f1cf13fd3127980987185a
Kaung Khant Kyaw <timmytseng1999@gmail.com>
2019-12-17 12:41:00 +0630

    Initial Commit

---
+++ RicePurityTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RicePurityTest2
+//
+//  Created by Timmy Tseng on 12/17/19.
+//  Copyright © 2019 kaungkhantkyaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
